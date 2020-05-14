68d3a7ea6cc0e66e7b7e3f75ca81e63b2bd3a906
Isaac (Pi) <locke19hegemon@gmail.com>
2020-05-14 09:33:25 -0700

    receiver works on pi

--- test_rec.py
+++ test_rec.py
@@ -12,9 +12,9 @@ imageHub = imagezmq.ImageHub()
 # initialize the list of class labels MobileNet SSD was trained to
 # detect, then generate a set of bounding box colors for each class
 CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
-	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
-	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
-	"sofa", "train", "tvmonitor"]
+    "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+    "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+    "sofa", "train", "tvmonitor"]
 
 # load our serialized model from disk
 print("[INFO] loading model...")
@@ -44,111 +44,81 @@ ACTIVE_CHECK_SECONDS = ESTIMATED_NUM_PIS * ACTIVE_CHECK_PERIOD
 mW = 2
 mH = 2
 print("[INFO] detecting: {}...".format(", ".join(obj for obj in
-	CONSIDER)))
+    CONSIDER)))
 
 # start looping over all the frames
 while True:
-	# receive RPi name and frame from the RPi and acknowledge
-	# the receipt
-	(rpiName, frame) = imageHub.recv_image()
-	imageHub.send_reply(b'OK')
+    # receive RPi name and frame from the RPi and acknowledge
+    # the receipt
+    (rpiName, frame) = imageHub.recv_image()
+    imageHub.send_reply(b'OK')
 
-	# if a device is not in the last active dictionary then it means
-	# that its a newly connected device
-	if rpiName not in lastActive.keys():
-		print("[INFO] receiving data from {}...".format(rpiName))
+    # if a device is not in the last active dictionary then it means
+    # that its a newly connected device
+    if rpiName not in lastActive.keys():
+        print("[INFO] receiving data from {}...".format(rpiName))
 
-	# record the last active time for the device from which we just
-	# received a frame
-	lastActive[rpiName] = datetime.now()
+    # record the last active time for the device from which we just
+    # received a frame
+    lastActive[rpiName] = datetime.now()
 
-	# resize the frame to have a maximum width of 400 pixels, then
-	# grab the frame dimensions and construct a blob
-	frame = imutils.resize(frame, width=400)
-	(h, w) = frame.shape[:2]
-	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
-		0.007843, (300, 300), 127.5)
+    # resize the frame to have a maximum width of 400 pixels, then
+    # grab the frame dimensions and construct a blob
+    frame = imutils.resize(frame, width=400)
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+        0.007843, (300, 300), 127.5)
 
-	# pass the blob through the network and obtain the detections and
-	# predictions
-	net.setInput(blob)
-	detections = net.forward()
+    # pass the blob through the network and obtain the detections and
+    # predictions
+    #net.setInput(blob)
+    #detections = net.forward()
 
-	# reset the object count for each object in the CONSIDER set
-	objCount = {obj: 0 for obj in CONSIDER}
+    # reset the object count for each object in the CONSIDER set
+    objCount = {obj: 0 for obj in CONSIDER}
 
-	# loop over the detections
-	for i in np.arange(0, detections.shape[2]):
-		# extract the confidence (i.e., probability) associated with
-		# the prediction
-		confidence = detections[0, 0, i, 2]
+    # draw the sending device name on the frame
+    cv2.putText(frame, rpiName, (10, 25),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
-		# filter out weak detections by ensuring the confidence is
-		# greater than the minimum confidence
-		if confidence > args["confidence"]:
-			# extract the index of the class label from the
-			# detections
-			idx = int(detections[0, 0, i, 1])
+    # draw the object count on the frame
+    label = ", ".join("{}: {}".format(obj, count) for (obj, count) in
+        objCount.items())
+    cv2.putText(frame, label, (10, h - 20),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255,0), 2)
 
-			# check to see if the predicted class is in the set of
-			# classes that need to be considered
-			if CLASSES[idx] in CONSIDER:
-				# increment the count of the particular object
-				# detected in the frame
-				objCount[CLASSES[idx]] += 1
+    # update the new frame in the frame dictionary
+    frameDict[rpiName] = frame
 
-				# compute the (x, y)-coordinates of the bounding box
-				# for the object
-				box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-				(startX, startY, endX, endY) = box.astype("int")
+    # build a montage using images in the frame dictionary
+    montages = build_montages(frameDict.values(), (w, h), (mW, mH))
 
-				# draw the bounding box around the detected object on
-				# the frame
-				cv2.rectangle(frame, (startX, startY), (endX, endY),
-					(255, 0, 0), 2)
+    # display the montage(s) on the screen
+    for (i, montage) in enumerate(montages):
+        cv2.imshow("Home pet location monitor ({})".format(i),
+            montage)
 
-	# draw the sending device name on the frame
-	cv2.putText(frame, rpiName, (10, 25),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    # detect any kepresses
+    key = cv2.waitKey(1) & 0xFF
 
-	# draw the object count on the frame
-	label = ", ".join("{}: {}".format(obj, count) for (obj, count) in
-		objCount.items())
-	cv2.putText(frame, label, (10, h - 20),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255,0), 2)
+    # if current time *minus* last time when the active device check
+    # was made is greater than the threshold set then do a check
+    if (datetime.now() - lastActiveCheck).seconds > ACTIVE_CHECK_SECONDS:
+        # loop over all previously active devices
+        for (rpiName, ts) in list(lastActive.items()):
+            # remove the RPi from the last active and frame
+            # dictionaries if the device hasn't been active recently
+            if (datetime.now() - ts).seconds > ACTIVE_CHECK_SECONDS:
+                print("[INFO] lost connection to {}".format(rpiName))
+                lastActive.pop(rpiName)
+                frameDict.pop(rpiName)
 
-	# update the new frame in the frame dictionary
-	frameDict[rpiName] = frame
+        # set the last active check time as current time
+        lastActiveCheck = datetime.now()
 
-	# build a montage using images in the frame dictionary
-	montages = build_montages(frameDict.values(), (w, h), (mW, mH))
-
-	# display the montage(s) on the screen
-	for (i, montage) in enumerate(montages):
-		cv2.imshow("Home pet location monitor ({})".format(i),
-			montage)
-
-	# detect any kepresses
-	key = cv2.waitKey(1) & 0xFF
-
-	# if current time *minus* last time when the active device check
-	# was made is greater than the threshold set then do a check
-	if (datetime.now() - lastActiveCheck).seconds > ACTIVE_CHECK_SECONDS:
-		# loop over all previously active devices
-		for (rpiName, ts) in list(lastActive.items()):
-			# remove the RPi from the last active and frame
-			# dictionaries if the device hasn't been active recently
-			if (datetime.now() - ts).seconds > ACTIVE_CHECK_SECONDS:
-				print("[INFO] lost connection to {}".format(rpiName))
-				lastActive.pop(rpiName)
-				frameDict.pop(rpiName)
-
-		# set the last active check time as current time
-		lastActiveCheck = datetime.now()
-
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
 
 # do a bit of cleanup
 cv2.destroyAllWindows()
