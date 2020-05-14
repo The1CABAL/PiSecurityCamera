066300479145b0a4f63d64620f0e7ec6788e61d4
Isaac Bly <locke19hegemon@gmail.com>
2020-05-14 08:00:17 -0700

    Pi Test

--- Start_Camera.py
+++ Start_Camera.py
@@ -1,22 +1,23 @@
 
+import base64
 import cv2
-import numpy as np
-import socket
-import sys
-import pickle
-import struct
+import zmq
 
-cap=cv2.VideoCapture(0)
-clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-clientsocket.connect(('192.168.1.16',8089))
+context = zmq.Context()
+footage_socket = context.socket(zmq.PUB)
+footage_socket.connect('tcp://localhost:5555')
+
+camera = cv2.VideoCapture(0)  # init the camera
 
 while True:
-    ret,frame=cap.read()
-    # Serialize frame
-    data = pickle.dumps(frame)
+    try:
+        grabbed, frame = camera.read()  # grab the current frame
+        frame = cv2.resize(frame, (640, 480))  # resize the frame
+        encoded, buffer = cv2.imencode('.jpg', frame)
+        jpg_as_text = base64.b64encode(buffer)
+        footage_socket.send(jpg_as_text)
 
-    # Send message length first
-    message_size = struct.pack("L", len(data)) ### CHANGED
-
-    # Then data
-    clientsocket.sendall(message_size + data)
+    except KeyboardInterrupt:
+        camera.release()
+        cv2.destroyAllWindows()
+        break
