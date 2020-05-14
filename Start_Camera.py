b2752cb7c10516567dd8508c8138c885d44f7515
Isaac Bly <locke19hegemon@gmail.com>
2020-05-13 17:36:52 -0700

    push test file to pi

--- Start_Camera.py
+++ Start_Camera.py
@@ -1,22 +1,14 @@
 
-import Data.GlobalVars as cfg
-try:
-    import PiCamera
-    cfg.isPi = 1
-except:
-    cfg.isPi = 0
-
+import cv2
+import numpy as np
 import socket
-import time
-from imutils.video import VideoStream
-import imagezmq
-
-ip = input("Input Server IP Address: ")
-sender = imagezmq.ImageSender(connect_to='tcp://'+ip+':5555')
- 
-rpi_name = socket.gethostname() # send RPi hostname with each image
-picam = VideoStream(usePiCamera=True).start()
-time.sleep(2.0)  # allow camera sensor to warm up
-while True:  # send images as stream until Ctrl-C
-    image = picam.read()
-    sender.send_image(rpi_name, image)
+import sys
+import pickle
+import struct
+cap=cv2.VideoCapture(0)
+clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+clientsocket.connect(('localhost',8089))
+while True:
+    ret,frame=cap.read()
+    data = pickle.dumps(frame)
+    clientsocket.sendall(struct.pack("H", len(data))+data)
