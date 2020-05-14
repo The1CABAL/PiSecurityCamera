f95b6b16744b50633b5da3ce5ee42030f1828f65
Isaac Bly <locke19hegemon@gmail.com>
2020-05-14 08:54:27 -0700

    test

--- Start_Camera.py
+++ Start_Camera.py
@@ -1,65 +1,29 @@
 
-from socket import *
-import cv2
-import numpy
-from getpass import getpass
+from imutils.video import VideoStream
+import imagezmq
+import argparse
+import socket
+import time
 
-n = 25
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-s", "--server-ip", required=True,
+	help="ip address of the server to which the client will connect")
+args = vars(ap.parse_args())
 
-def recvall(conn, count):
-    buf = b''
-    while count:
-        newbuf = conn.recv(count)
-        if not newbuf:
-            return None
-        buf += newbuf
-        count -= len(newbuf)
+# initialize the ImageSender object with the socket address of the
+# server
+sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(
+	args["server_ip"]))
 
-    return buf
-
-try:
-    sock = socket() # Create a socket object
-    print('Hello')
-    ip = raw_input('Enter IP address of server: ')
-    port = input('Enter Port number of server: ')
-    print('==' * n)
-    sock.connect((ip, port))
-    
-
-    data = sock.recv(1024)
-    print(data)
-    ID = raw_input('Enter your ID: ')
-    passd = getpass('Enter yur password: ')
-
-    print('==' * n)
-    
-    sock.send(ID+':'+passd)
-
-    data = sock.recv(1024)
-    if data == 'Denied':
-        print('Access Denied...')
-        exit()
-
-    while 1:
-        length = recvall(sock, 16)
-        if length == None:
-            break
-        
-        buf = recvall(sock, int(length))
-        data = numpy.fromstring(buf, dtype='uint8')
-
-        decimg = cv2.imdecode(data, 1)
-        cv2.imshow('Client', decimg)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            sock.send('Quit')
-            break
-        else:
-            sock.send('OK')
-
-    sock.close()
-    cv2.destroyAllWindows()
-    
-except:
-    pass
-
-exit(1)
+# get the host name, initialize the video stream, and allow the
+# camera sensor to warmup
+rpiName = socket.gethostname()
+vs = VideoStream(usePiCamera=True).start()
+#vs = VideoStream(src=0).start()
+time.sleep(2.0)
+ 
+while True:
+	# read the frame from the camera and send it to the server
+	frame = vs.read()
+	sender.send_image(rpiName, frame)
