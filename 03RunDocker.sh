fa0baf299d574dd1b478c91abdbbe9eed5347487
Isaac Bly <locke19hegemon@gmail.com>
2020-07-25 15:27:06 -0700

    Added success check

--- 03RunDocker.sh
+++ 03RunDocker.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-sudo docker run -it -p 8000:8000 -p 5555:5555 --name picamserver -d picamserver
+sudo docker run -it -p 8000:8000 -p 5555:5555 --name picamserver -d picamserver && \ 
+sleep 5 && \
+sudo docker inspect -f {{.State.Running}} picamserver && \
+echo "Container is running."
