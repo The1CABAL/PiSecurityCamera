fc54d27adf54ad3fd4741cbf9f4783cc5437ace6
The1CABAL <37129528+The1CABAL@users.noreply.github.com>
2020-08-15 19:35:03 -0700

    Delete 03RunDocker.sh

--- 03RunDocker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-sudo docker run -it -p 8000:8000 -p 5555:5555 --name picamserver -d picamserver && \ 
-sleep 5 && \
-sudo docker inspect -f {{.State.Running}} picamserver && \
-echo "Container is running."
